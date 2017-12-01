2f863187d0403991998f7a8b423bf4e2039f16ec
Tony <tony@gmail.com>
2017-12-01 12:38:08 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/android/coolweather/com/coolweather/db/Province.java
@@ -0,0 +1,41 @@
+package android.coolweather.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by DELL on 2017/12/1.
+ */
+
+public class Province extends DataSupport{
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+
+}
